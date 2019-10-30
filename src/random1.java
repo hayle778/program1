d01652f92999bc5d128e769562f26edfcebb8588
yourUsername <yourEmail@domain.com>
2019-10-29 21:41:21 -0400

    Program1

---
+++ src/random1.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Scanner;
+
+public class random1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner( System.in );
+        int intArray[] = new int[10];
+        System.out.println( "Enter number" );
+
+        int userNo = 0;
+
+        for (int i = 0; i < 10; i++) {
+         System.out.println( "baby " );
+            (i) = 1 + userNo;
+            input.nextInt(10);
+
+
+
+      String  StringArray [] = {"belay, baby, haile, john, mike, naomi, haile, dave, abebe, kefa"}  ;
+
+
+System.out.println( "print a random name " );
+    input.nextLine();
+
+
+}
+        }
+    }
